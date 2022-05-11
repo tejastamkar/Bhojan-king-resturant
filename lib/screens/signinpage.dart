a6451e31e9c34d5c3fba7fc278870d0b32842607
tejastamkar <tejas.tamkar@gmail.com>
2022-05-11 17:29:48 +0530

    added fonts and init files

---
+++ lib/screens/signinpage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [SvgPicture.asset('icons/logo.svg')],
+        ),
+      ),
+    );
+  }
+}
